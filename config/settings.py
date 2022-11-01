adaed9486129d962d92d5abd6a2924fc6895a8a8
AF <100952220+ibnbayo@users.noreply.github.com>
2022-11-01 12:31:11 +0100

    heroku deploy settings

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ibnbayo-job.herokuapp.com']
 
 # Application definition
 
